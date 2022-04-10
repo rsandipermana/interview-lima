0230b964b89f260191e56aa1a153f58e9f1374a4
rsandipermana <r.sandipermana@gmail.com>
2022-04-10 21:30:45 +0700

    Create fraudulentActivityNotifications.php

---
+++ fraudulentActivityNotifications.php
@@ -0,0 +1,30 @@
+<?php
+ 
+// For input
+$size        = explode(' ', readline('test1:'));
+$expenditure = explode(' ', readline('test2:'));
+
+// Proccessing
+$n            = $size[0];
+$d            = $size[1];
+$totalCheck   = $n - $d;
+$notification = 0;
+
+for ($x = $d; $x < $n; $x++) { 
+    // median
+    $median = 0;
+    for ($y = 0; $y < $d ; $y++) { 
+        $number = $x - $d + $y;
+        $median = $median + $expenditure[$number];
+    }
+    $median = $median/$d;
+
+    // total notifications
+    if ($expenditure[$x] >= $median * 2) {
+        $notification = $notification + 1;
+    }
+}
+
+// For output
+echo $notification;
+?>
